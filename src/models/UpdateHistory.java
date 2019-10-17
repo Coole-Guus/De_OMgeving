f7e53c56512dca7c844227a2d7cbcd36b5f912a5
LeanderLoomans <s1115153@student.hsleiden.nl>
2019-10-17 11:26:45 +0200

    Update models

--- src/models/UpdateHistory.java
+++ src/models/UpdateHistory.java
@@ -2,7 +2,21 @@ package models;
 
 import views.Observer;
 
+import java.util.ArrayList;
+import java.util.Date;
+
 public class UpdateHistory implements Observable {
+
+    private ArrayList<UpdateMessage> messageList;
+
+    public void addMessage(String message, Date messageDate, String editorName) {
+        messageList.add(new UpdateMessage(message, messageDate, editorName));
+    }
+
+    public UpdateMessage getMessage(int index) {
+        return messageList.get(index);
+    }
+
     @Override
     public void registerObserver(Observer observer) {
 
