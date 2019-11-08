8632a71657abd702378c0eae0baf87266ee1506d
FinalDayz <steef2000d@gmail.com>
2019-11-08 10:23:49 +0100

    Details are also created when creating experiment

--- src/main/java/views/ExperimentAanmaakView.java
+++ src/main/java/views/ExperimentAanmaakView.java
@@ -17,6 +17,7 @@ import javafx.scene.layout.Pane;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
+import models.Details;
 import models.Experiment;
 import models.Observable;
 import javafx.scene.control.TextField;
@@ -105,7 +106,22 @@ public class ExperimentAanmaakView implements Observer{
 
         Experiment newExperiment = new Experiment(150, projectname, ts, fase, projectleader1, color);
         System.out.println(color);
-        (new HttpClientBuilder()).httpPostAdd(newExperiment, "experimenten", "create");
+        HttpClientBuilder requester = new HttpClientBuilder();
+        int experimentId = Integer.parseInt(
+                requester.httpPostAdd(newExperiment, "experimenten", "create")
+        );
+
+        Details experimentDetails = new Details();
+        experimentDetails.setExperimentId(experimentId);
+        experimentDetails.setNetwerk("");
+        experimentDetails.setBeschrijving("");
+        experimentDetails.setDoorlooptijd("");
+        experimentDetails.setKostenInovatie("");
+        experimentDetails.setKostenAnders("");
+        experimentDetails.setStatus("");
+        experimentDetails.setVoortgang("");
+        experimentDetails.setArchiefType(null);
+        requester.httpPostAdd(experimentDetails, "experimentDetails", "create");
 }
 
     public void show() {
